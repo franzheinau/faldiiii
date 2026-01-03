4002598ddf733bc1aead7abe48589247d5318881
franzheinau <rifaldi0823666@gamail.com>
2026-01-04 05:21:33 +0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <footer className="footer">
+        <p>&copy; 2024 My Website. All rights reserved.</p>
+    </footer>
+
+  
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function header() {
+  return (
+    <div>
+     <header className="header">
+          <nav>
+            <div className="logo">
+              <a href="/">Logo</a>
+            </div>
+          </nav>
+        </header>
+    </div>
+  )
+}
+
+export default header
